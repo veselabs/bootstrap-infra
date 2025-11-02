0e2fe2f31a214bfcd063a53085dcf95794dbabc2
Veselin Ivanov <me@veselyn.com>
2025-11-02 16:27:25 +0100

    feat: convert to bootstrap polyrepo (#1)

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    region       = "eu-north-1"
+    bucket       = "veselabs-terraform-state"
+    key          = "bootstrap/terraform.tfstate"
+    use_lockfile = true
+  }
+}
